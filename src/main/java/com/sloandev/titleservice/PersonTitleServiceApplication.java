36ccb6d6c2d6af1b38b504b42e49bc2b20a60e35
sloanibe <sloanibe@gmail.com>
2018-01-30 12:48:41 -0800

    Initial commit

---
+++ src/main/java/com/sloandev/titleservice/PersonTitleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sloandev.titleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonTitleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonTitleServiceApplication.class, args);
+	}
+}
